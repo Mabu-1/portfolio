ab00f6408b6444559826bc18e32be4b86653629d
Mabu1 <umahtab65@gmail.com>
2024-09-05 01:12:35 +0600

    typo fixed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
-import Home from "../Pages/Home/Hone/Home";
+import Home from "../Pages/Home/Home/Home";
 import NotFound from "../NotFound";
 const router = createBrowserRouter([
     {
